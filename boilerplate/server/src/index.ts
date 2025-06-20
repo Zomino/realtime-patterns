9fa624274c24435440cddf2a671df6c5bc84dda9
Zou Minowa <zou.minowa@shoppermediagroup.com>
2025-06-20 20:26:45 +0100

    Add workspaces for better dependency management

--- boilerplate/server/src/index.ts
+++ boilerplate/server/src/index.ts
@@ -1,10 +1,15 @@
-import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import express from 'express';
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello, Express!');
